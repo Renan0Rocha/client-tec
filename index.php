d3e6f98384c9d46cceedae73f628e6fc7d2819d3
Renan Rocha <102130210+Renan0Rocha@users.noreply.github.com>
2023-04-03 19:36:45 -0400

    update 03-04-23

--- index.php
+++ index.php
@@ -7,24 +7,8 @@
 require_once "system/config.php";
 include_once "helpers.php";
 
-$texto = 'texto para resumir';
-
-//echo $total = mb_strlen(str_replace(' ', '', $texto));
-
-echo $total = mb_strlen(trim($texto));
 echo '<hr>';
+//var_dump($_SERVER);
+echo dataAtual();
 
-/*
-$string = 'texto';
-$int = 10;
-$float = 10.999;
-$bool = true;
-$nulo = null;
 
-var_dump($texto);
-echo '<hr>';
-echo saudacao();
-echo "<hr>";
-echo resumirTexto($texto, 100, 'contiue');
-echo "<hr>";
-*/
--- system/config.php
+++ system/config.php
@@ -1,6 +1,13 @@
 <?php
-include_once "config.php";
-
-
 //Arquivo de configuração do sistema
+
+//Definir fuso horário
 date_default_timezone_set("America/Porto_Velho");
+
+//Informações do site
+define('SITE_NOME', 'UnSet');
+define('SITE_DESCRICAO', 'UnSet - Tecnologia em Sistemas');
+
+//Informações do sitema
+define('URL_PRODUCAO', 'http://unset.com.br');
+define('URL_DESENVOLVIMENTO', 'http://localhost/blog');
